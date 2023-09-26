4b2a4591ce29ea378ab6ffa1d98686fde2529134
AELISBAN <109315353+AELISBAN@users.noreply.github.com>
2023-09-25 22:51:10 -0500

    Add files via upload

---
+++ Models/Productos.cs
@@ -0,0 +1,13 @@
+namespace mimvc.Models
+{
+    public class Productos
+    {
+        public int Id {get; set;}
+        public string Nombre {get; set;}
+        public string Descripcion {get; set;}
+        public decimal Precio {get; set;}
+        public Boolean Activo {get; set;}
+        public DateTime Fecha {get; set;}
+
+    }
+}
